f968de2846763585f039660a27dc21410c8665b1
shying li <shyingli@yujinsuodeiMac.local>
2017-04-10 17:50:23 +0800

    room

---
+++ DouYuAPP/DouYuAPP/Classes/Room/Controller/Yo_RoomNormalViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Yo_RoomNormalViewController.swift
+//  DouYuAPP
+//
+//  Created by shying li on 2017/4/10.
+//  Copyright © 2017年 李世洋. All rights reserved.
+//
+
+import UIKit
+
+class Yo_RoomNormalViewController: GenericViewController<Yo_RoomNormalContentView> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+
+}
